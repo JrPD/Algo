f0d54cff30951ac744223a233f9a206f0a1dafd9
JrPD <tsurkan_i@mail.ru>
2016-02-03 22:10:22 +0200

    binary tree search

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,29 @@ namespace Algorithms
 {
 	class Program
 	{
+		public static int BinarySearch(int[] values, int key)
+		{
+			int l0 = 0;
+			int hi = values.Length - 1;
+
+			while (l0<=hi)
+			{
+				var mid = (l0+hi)/2;
+				if (key < values[mid])
+					hi = mid-1;
+				else if (key > mid)
+					l0 = mid+1;
+				else
+					return mid;
+			}
+			return -1;
+		}
 		static void Main(string[] args)
 		{
+			int[] val = new[] {1, 4,8,16,18,27,34,58,79,84,85,88,89,91};
+			int key = 98;
+
+			BinarySearch(val, key);
 		}
 	}
 }
